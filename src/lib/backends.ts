2c29f7b7765e315428c4f04739e2afda85ae5263
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-08 23:12:59 +0100

    Re-defined some of the inputs

--- src/lib/backends.ts
+++ src/lib/backends.ts
@@ -5,12 +5,11 @@ const AWS_REGION = process.env.AWS_REGION;
 const AWS_TERRAFORM_BUCKET = process.env.AWS_TERRAFORM_BUCKET;
 const ENVIRONMENT = process.env.ENVIRONMENT;
 const SERVICE_NAME = process.env.SERVICE_NAME;
-const FILE_NAME = process.env.TERRAFORM_STATE_FILE_NAME;
 
 export const buildS3Backend = (scope: Construct): S3Backend => {
   return new S3Backend(scope, <S3BackendProps>{
     bucket: AWS_TERRAFORM_BUCKET,
-    key: `${ENVIRONMENT}/${SERVICE_NAME}/${FILE_NAME}.tf`,
+    key: `${ENVIRONMENT}/${SERVICE_NAME}/aws-s3-website.tf`,
     region: AWS_REGION,
     acl: "bucket-owner-full-control"
   });
