1d36f180b7dffb86de85497a60345080fdf9540f
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-09 14:17:59 +0100

    Improvements and logging (#13)

--- lib/backends.ts
+++ lib/backends.ts
@@ -1,15 +1,11 @@
 import { Construct } from "constructs";
 import { S3Backend, S3BackendProps } from "cdktf";
+import { AWS_REGION, AWS_TERRAFORM_BUCKET, ENVIRONMENT, SERVICE_NAME, TERRAFORM_STATE_FILE_NAME } from "../config";
 
-const AWS_REGION = process.env.AWS_REGION;
-const AWS_TERRAFORM_BUCKET = process.env.AWS_TERRAFORM_BUCKET;
-const ENVIRONMENT = process.env.ENVIRONMENT;
-const SERVICE_NAME = process.env.SERVICE_NAME;
-
-export const buildS3Backend = (scope: Construct, fileName: string): S3Backend => {
+export const buildS3Backend = (scope: Construct): S3Backend => {
   return new S3Backend(scope, <S3BackendProps>{
     bucket: AWS_TERRAFORM_BUCKET,
-    key: `${ENVIRONMENT}/${SERVICE_NAME}/${fileName}.tf`,
+    key: `${ENVIRONMENT}/${SERVICE_NAME}/${TERRAFORM_STATE_FILE_NAME}.tf`,
     region: AWS_REGION,
     acl: "bucket-owner-full-control"
   });
