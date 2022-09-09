33c589f85ea3bab72a15e42c028cfaeaa113e706
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-09-09 14:33:39 +0100

    Fixed wrong record name and Cloudfront caching rules (#15)

--- config.ts
+++ config.ts
@@ -14,4 +14,5 @@ export const SUBDOMAIN = process.env.SUBDOMAIN;
 export const DOMAIN = process.env.DOMAIN;
 export const REDIRECT = process.env.REDIRECT;
 
+export const IS_PRODUCTION = ENVIRONMENT === "production";
 export const BUCKET_NAME = process.env.SUBDOMAN !== "" ? `${SUBDOMAIN}.${DOMAIN}` : DOMAIN;
